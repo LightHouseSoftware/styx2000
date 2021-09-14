e01ed0dfac3a4058054fa09fc7c20622c52c6e4d
Oleg Bakharev <disconnectix@gmail.com>
2021-09-15 00:01:28 +0300

    Added new module msgrt to extrautil package

---
+++ source/styx2000/extrautil/msgrt.d
@@ -0,0 +1,136 @@
+module styx2000.extrautil.msgrt;
+
+private {
+	import styx2000.protoconst.messages;
+	import styx2000.protobj.type;
+}
+
+// transform to R-message type
+auto toRtype(Type type)
+{		
+	auto rtype = new Type;	
+	with (STYX_MESSAGE_TYPE)
+	{
+		switch(type.getType) {
+			// version
+			case T_VERSION:
+				rtype.setType(R_VERSION);
+				break;
+			// auth
+			case T_AUTH:
+				rtype.setType(R_AUTH);
+				break;
+			// flush
+			case T_FLUSH:
+				rtype.setType(R_FLUSH);
+				break;
+			// attach
+			case T_ATTACH:
+				rtype.setType(R_ATTACH);
+				break;
+			// walk
+			case T_WALK:
+				rtype.setType(R_WALK);
+				break;
+			case T_OPEN:
+				rtype.setType(R_OPEN);
+				break;
+			// create
+			case T_CREATE:
+				rtype.setType(R_CREATE);
+				break;
+			// read
+			case T_READ:
+				rtype.setType(R_READ);
+				break;
+			// write
+			case T_WRITE:
+				rtype.setType(R_WRITE);
+				break;
+			// clunk
+			case T_CLUNK:
+				rtype.setType(R_CLUNK);
+				break;
+			// remove
+			case T_REMOVE:
+				rtype.setType(R_REMOVE);
+				break;
+			// stat
+			case T_STAT:
+				rtype.setType(R_STAT);
+				break;
+			// wstat
+			case T_WSTAT:
+				rtype.setType(R_WSTAT);
+				break;
+			default:
+				throw new Exception("Wrong message type used to get R-message type");
+		}
+	}
+	return rtype;
+}
+
+// transform to T-message type
+auto toTtype(Type type)
+{		
+	auto ttype = new Type;	
+	with (STYX_MESSAGE_TYPE)
+	{
+		switch(type.getType) {
+			// version
+			case R_VERSION:
+				ttype.setType(T_VERSION);
+				break;
+			// auth
+			case R_AUTH:
+				ttype.setType(T_AUTH);
+				break;
+			// flush
+			case R_FLUSH:
+				ttype.setType(T_FLUSH);
+				break;
+			// attach
+			case R_ATTACH:
+				ttype.setType(T_ATTACH);
+				break;
+			// walk
+			case R_WALK:
+				ttype.setType(T_WALK);
+				break;
+			case R_OPEN:
+				ttype.setType(T_OPEN);
+				break;
+			// create
+			case R_CREATE:
+				ttype.setType(T_CREATE);
+				break;
+			// read
+			case R_READ:
+				ttype.setType(T_READ);
+				break;
+			// write
+			case R_WRITE:
+				ttype.setType(T_WRITE);
+				break;
+			// clunk
+			case R_CLUNK:
+				ttype.setType(T_CLUNK);
+				break;
+			// remove
+			case R_REMOVE:
+				ttype.setType(T_REMOVE);
+				break;
+			// stat
+			case R_STAT:
+				ttype.setType(T_STAT);
+				break;
+			// wstat
+			case R_WSTAT:
+				ttype.setType(T_WSTAT);
+				break;
+			default:
+				throw new Exception("Wrong message type used to get T-message type");
+		}
+	}
+	return ttype;
+}
--- source/styx2000/extrautil/package.d
+++ source/styx2000/extrautil/package.d
@@ -4,4 +4,5 @@ public {
 	import styx2000.extrautil.dir;
 	import styx2000.extrautil.dirstat;
 	import styx2000.extrautil.msgranges;
+	import styx2000.extrautil.msgrt;
 }
