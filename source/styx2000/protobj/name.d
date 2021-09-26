c251af82969253a4c01c5cdc2e6ba46e377deab7
Oleg Bakharev <disconnectix@gmail.com>
2021-09-26 15:28:35 +0300

    Added documentation for name module

--- source/styx2000/protobj/name.d
+++ source/styx2000/protobj/name.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+A type for representing the name object of the 9P / Styx protocol. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protobj.name;
 
 private {
@@ -9,7 +19,11 @@ private {
 	import styx2000.protobj.styxobject;
 }
 
-// user name
+/**
+	A class that provides a type for the name field in some Styx messages. Inherits methods from the Name class and the StyxObject class. 
+	See_Also:
+		https://web.archive.org/web/20201029184954/https://powerman.name/Inferno/man/5/0intro.html
+*/
 class Name : StyxObject
 {
 	protected {
@@ -18,40 +32,50 @@ class Name : StyxObject
 		ubyte[] _representation;
 	}
 	
-	// create from value
+	/**
+	A constructor that creates an object of the Name class with the given parameter in the form of some string value representing names. 
+	If called without parameters, then the default parameter is empty string value. 
+    Params:
+    name = String value for name.
+    
+    Typical usage:
+    ----
+    Name name = new Name(`test`);
+    ----
+    */
 	this(string name = "")
 	{
 		_name = name;
 		_representation = VariableLengthSequence.pack(cast(ubyte[]) name);
 	}
 	
-	// getter
+	/// Get name from Name object
 	string getName()
 	{
 		return _name;
 	}
 	
-	// setter
+	/// Set mode from string object
 	void setName(string name)
 	{
 		_name = name;
 		_representation = VariableLengthSequence.pack(cast(ubyte[]) name);
 	}
 	
-	// pack to bytes array
+	/// Pack to bytes array
 	ubyte[] pack()
 	{
 		return _representation;
 	}
 	
-	// unpack from bytes array
+	/// Unpack from bytes array
 	void unpack(ubyte[] bytes...)
 	{
 		_name = cast(string) (VariableLengthSequence.unpack(bytes));
 		_representation = VariableLengthSequence.pack(cast(ubyte[]) _name);
 	}
 	
-	// string representation
+	/// Convenient string representation of an object for printing 
 	override string toString()
 	{
 		return format(
@@ -60,5 +84,6 @@ class Name : StyxObject
 		);
 	}
 	
+	/// An alias for easier packing into a byte array without having to manually call the pack() method
 	alias pack this;
 }
