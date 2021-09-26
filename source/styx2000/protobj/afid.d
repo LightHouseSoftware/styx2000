eb7cb8f8d228554d9559ca300e0fa5c31eccbc64
Oleg Bakharev <disconnectix@gmail.com>
2021-09-26 12:49:58 +0300

    Added documentation for afid module

--- source/styx2000/protobj/afid.d
+++ source/styx2000/protobj/afid.d
@@ -1,3 +1,14 @@
+// Written in the D programming language.
+
+/**
+A type for representing the afid object of the 9P / Styx protocol. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
+
 module styx2000.protobj.afid;
 
 private {
@@ -10,18 +21,32 @@ private {
 	import styx2000.protobj.fid;
 }
 
-// file identificator (afid) 
+/**
+	A class that provides a type for the afid field in some Styx messages. Inherits methods from the Fid class and the styxObject class. 
+*/
 class Afid : Fid
 {
+	/**
+	A constructor that creates an object of the Afid class with the given parameter in the form of some integer value representing fid. 
+	If called without parameters, then the default parameter is the STYX_NOFID value from styx2000.protoconst.base. 
+    Params:
+    fid = Unique 32-bit value assigned by the Styx client.
+    Typical usage:
+    ----
+    Afid afid = new Afid(0);
+    ----
+    */
 	this(uint fid = STYX_NOFID)
 	{
 		super(fid);
 	}
 	
+	/// An alias that allows you to call a getter method without accessing the base Fid class
 	alias getAfid = getFid;	
+	/// An alias that allows you to call a setter method without accessing the base Fid class
 	alias setAfid = setFid;	
 	
-	// string representation
+	/// Convenient string representation of an object for printing 
 	override string toString()
 	{
 		return format(
@@ -30,5 +55,6 @@ class Afid : Fid
 		);
 	}
 	
+	/// An alias for easier packing into a byte array without having to manually call the pack () method
 	alias pack this;
 }
--- source/styx2000/protobj/package.d
+++ source/styx2000/protobj/package.d
@@ -11,18 +11,22 @@ Authors:   Oleg Bakharev,
 module styx2000.protobj;
 
 public {
+	
 	/**
 		The class required to represent the `afid` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.afid;
+	
 	/**
 		The class required to represent the `aqid` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.aqid;
+	
 	/**
 		The class required to represent the `aname` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.aname;
+	
 	/**
 		The class required to represent the `count` object of the Styx protocol. 
 	*/
@@ -31,30 +35,37 @@ public {
 		The class required to represent the `data` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.data;
+	
 	/**
 		The class required to represent the `ename` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.ename;
+	
 	/**
 		The class required to represent the `fid` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.fid;
+	
 	/**
 		The class required to represent the `iounit` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.iounit;
+	
 	/**
 		The class required to represent the `mode` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.mode;
+	
 	/**
 		The class required to represent the `msize` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.msize;
+	
 	/**
 		The class required to represent the `name` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.name;
+	
 	/**
 		The class required to represent the `newfid` object of the Styx protocol. 
 	*/
@@ -67,42 +78,52 @@ public {
 		The class required to represent the `nwqid` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.nwqid;
+	
 	/**
 		The class required to represent the `qid` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.qid;
+	
 	/**
 		The class required to represent the `oldtag` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.oldtag;
+	
 	/**
 		The class required to represent the `offset` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.offset;
+	
 	/**
 		The class required to represent the `perm` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.perm;
+	
 	/**
 		The class required to represent the `size` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.size;
+	
 	/**
 		The class required to represent the `stat` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.stat;
+	
 	/**
 		The class required to represent the `version` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.styxversion;
+	
 	/**
 		The class required to represent the `tag` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.tag;
+	
 	/**
 		The class required to represent the `type` object of the Styx protocol. 
 	*/
 	import styx2000.protobj.type;
+	
 	/**
 		The class required to represent the `uname` object of the Styx protocol. 
 	*/
