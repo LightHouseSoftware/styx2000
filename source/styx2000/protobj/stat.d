b76842df68903dc2bfa7936642c02ef9bfb06495
Oleg Bakharev <disconnectix@gmail.com>
2021-09-26 23:16:37 +0300

    Added documentation for stat module

--- source/styx2000/protobj/stat.d
+++ source/styx2000/protobj/stat.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+A type for representing the stat object of the 9P / Styx protocol. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protobj.stat;
 
 private {
@@ -15,6 +25,12 @@ public {
 	import styx2000.protobj.qid;
 }
 
+
+/**
+	A class that provides a type for the stat field in some Styx messages. Inherits methods from the StyxObject class. 
+	See_Also:
+		https://web.archive.org/web/20201029184954/https://powerman.name/Inferno/man/5/0intro.html
+*/
 class Stat : StyxObject
 {
 	protected {
@@ -53,6 +69,57 @@ class Stat : StyxObject
 		}
 	}
 	
+	/**
+	A constructor that creates an object of the Stat class with the given parameters. 
+	If called without parameters, then the default parameter for type, dev, atime, mtime, length is zero;
+	default value for Perm and Qid parameter is standard initialized values for their (see Perm and Qid
+	classes); the default parameter for name, uid, gid and muid is empty string.
+	Params:
+    type = Major server version.
+    dev = Minor server version.
+    qid = Unique server id for filesystem object.
+    perm = Permission.
+    atime = Last acess time (in Unix epoch format).
+    mtime = Last modification time (in Unix epoch format).
+    length = Filesystem object size (in bytes).
+    name = Filesystem object name.
+    uid = Owner name.
+    gid = Group of owner name.
+    muid = Name of user, who modified file.
+    
+    Typical usage:
+    ----
+    // chmod 775 (drwxrwxr-x)
+		Perm perm = new Perm;
+		perm.setPerm(
+			STYX_FILE_PERMISSION.DMDIR | STYX_FILE_PERMISSION.OWNER_EXEC | STYX_FILE_PERMISSION.OWNER_READ | STYX_FILE_PERMISSION.OWNER_WRITE |
+			STYX_FILE_PERMISSION.GROUP_READ | STYX_FILE_PERMISSION.GROUP_WRITE | STYX_FILE_PERMISSION.GROUP_EXEC | 
+			STYX_FILE_PERMISSION.OTHER_READ | STYX_FILE_PERMISSION.OTHER_EXEC 
+		);
+			
+		Stat stat = new Stat(
+			// type and dev for kernel use (taken from some experiments with styxdecoder, see above)
+			77, 4,
+			new Qid,
+			// permissions
+			perm,
+			// access time
+			123456789,
+			// modification time
+			123456,
+			// conventional length for all directories is 0
+			0,
+			// file name (this, directory name)
+			"test",
+			// user name (owner of file)
+			"user",
+			// user group name
+			"users",
+			// others group name
+			""
+		);
+    ----
+    */
 	this(
 		ushort  type    = 0,
 		uint    dev     = 0,
@@ -109,136 +176,156 @@ class Stat : StyxObject
 		);
 	}
 	
-	// setters
+	/// Set type from unsigned value 
 	void setType(ushort type)
 	{
 		_type = type;
 		_representation[4..6] = toLEBytes!ushort(type);
 	}
 	
+	/// Set device from unsigned value 
 	void setDev(uint dev)
 	{
 		_dev = dev;
 		_representation[6..10] = toLEBytes!uint(dev);
 	}
 	
+	/// Set Qid from Qid type 
 	void setQid(Qid qid)
 	{
 		_qid = qid;
 		_representation[10..23] = qid.pack;
 	}
 	
+	/// Set mode from Perm type
 	void setMode(Perm mode)
 	{
 		_mode = mode;
 		_representation[23..27] = mode.pack;
 	}
 	
+	/// Set access time from unsigned value 
 	void setAtime(uint atime)
 	{
 		_atime = atime;
 		_representation[27..31] = toLEBytes!uint(atime);
 	}
 	
+	/// Set modification time from unsigned value 
 	void setMtime(uint mtime)
 	{
 		_mtime = mtime;
 		_representation[31..35] = toLEBytes!uint(mtime);
 	}
 	
+	/// Set size (in bytes) from unsigned value 
 	void setLength(ulong length)
 	{
 		_length = length;
 		_representation[35..43] = toLEBytes!ulong(length);
 	}
 	
+	/// Set name from string
 	void setName(string name)
 	{
 		_name = name;
 		updateVLSFields;
 	}
 	
+	/// Set user name from string 
 	void setUid(string uid)
 	{
 		_uid = uid;
 		updateVLSFields;
 	}
 	
+	/// Set user group name from string
 	void setGid(string gid)
 	{
 		_gid = gid;
 		updateVLSFields;
 	}
 	
+	/// Set modificator's name from string
 	void setMuid(string muid)
 	{
 		_muid = muid;
 		updateVLSFields;
 	}
 	
-	// getters
+	/// Get type from Stat object
 	ushort getType()
 	{
 		return _type;
 	}
 	
+	/// Get device from Stat object
 	uint getDev()
 	{
 		return _dev;
 	}
 	
+	/// Get Qid from Stat object
 	Qid getQid()
 	{
 		return _qid;
 	}
 	
+	/// Get mode from Stat object
 	Perm getMode()
 	{
 		return _mode;
 	}
 	
+	/// Get access time from Stat object
 	uint getAtime()
 	{
 		return _atime;
 	}
 	
+	/// Get modification time from Stat object
 	uint getMtime()
 	{
 		return _mtime;
 	}
 	
+	/// Get size from Stat object
 	ulong getLength()
 	{
 		return _length;
 	}
 	
+	/// Get name from Stat object
 	string getName()
 	{
 		return _name;
 	}
 	
+	/// Get user name from Stat object
 	string getUid()
 	{
 		return _uid;
 	}
 	
+	/// Get user group name from Stat object
 	string getGid()
 	{
 		return _gid;
 	}
 	
+	/// Get modificator's name from Stat object
 	string getMuid()
 	{
 		return _muid;
 	}
 	
-	// pack to bytes array
+	/// Pack to bytes array
 	ubyte[] pack()
 	{
 		return _representation;
 	}
 	
-	// unpack from bytes array
+	/// Unpack from bytes array
 	void unpack(ubyte[] bytes...)
 	{
 		_representation = bytes;
@@ -265,7 +352,7 @@ class Stat : StyxObject
 		_muid = cast(string) VariableLengthSequence.unpack(_content[vlsPosition..$]);
 	}
 	
-	// string representation
+	/// Convenient string representation of an object for printing
 	override string toString()
 	{
 		return format(
@@ -284,5 +371,6 @@ class Stat : StyxObject
 		);
 	}
 	
+	/// An alias for easier packing into a byte array without having to manually call the pack() method
 	alias pack this;
 }
