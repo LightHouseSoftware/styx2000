4b8b15f80380331eb33bcbb01da5a64c87c1f320
Oleg Bakharev <disconnectix@gmail.com>
2021-09-26 21:11:19 +0300

    Added documentation for size module

--- source/styx2000/protobj/size.d
+++ source/styx2000/protobj/size.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+A type for representing the size object of the 9P / Styx protocol. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protobj.size;
 
 private {
@@ -8,7 +18,11 @@ private {
 	import styx2000.protobj.styxobject;
 }
 
-// tag identificator
+/**
+	A class that provides a type for the size field in some Styx messages. Inherits methods from StyxObject class. 
+	See_Also:
+		https://web.archive.org/web/20201029184954/https://powerman.name/Inferno/man/5/0intro.html
+*/
 class Size : StyxObject
 {
 	protected {
@@ -17,44 +31,55 @@ class Size : StyxObject
 		ubyte[] _representation;
 	}
 	
-	// create from value
+	/**
+	A constructor that creates an object of the Size class with the given parameter in the form of unsigned value. 
+	If called without parameters, then the default parameter is zero.
+	Params:
+    size = Number of unsigned bytes.
+    
+    Typical usage:
+    ----
+    Size offset = new Size(17);
+    ----
+    */
 	this(uint size = 0)
 	{
 		_size = size;
 		_representation = toLEBytes!uint(size);
 	}
 	
-	// getter
+	/// Get size from Size object
 	uint getSize()
 	{
 		return _size;
 	}
 	
-	// setter
+	/// Set size from unsigned value
 	void setSize(uint size)
 	{
 		_size = size;
 		_representation = toLEBytes!uint(size);
 	}
 	
-	// pack to bytes array
+	/// Pack to bytes array
 	ubyte[] pack()
 	{
 		return _representation;
 	}
 	
-	// unpack from bytes array
+	/// Unpack from bytes array
 	void unpack(ubyte[] bytes...)
 	{
 		_representation = bytes[0..4];
 		_size = fromLEBytes!uint(bytes[0..4]);
 	}
 	
-	// string representation
+	/// Convenient string representation of an object for printing 
 	override string toString()
 	{
 		return format(`Size(size=%d)`, _size);
 	}
 	
+	/// An alias for easier packing into a byte array without having to manually call the pack() method
 	alias pack this;
 }
