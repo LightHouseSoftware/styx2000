9058b6553d68190722a2a947e810e081168ab645
Oleg Bakharev <disconnectix@gmail.com>
2021-09-26 23:41:57 +0300

    Added documentation for type module

--- source/styx2000/protobj/type.d
+++ source/styx2000/protobj/type.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+A type for representing the type object of the 9P / Styx protocol. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protobj.type;
 
 private {
@@ -10,7 +20,11 @@ public {
 	import styx2000.protoconst.messages;
 }
 
-// message type
+/**
+	A class that provides a type for the type field in some Styx messages. Inherits methods from StyxObject class. 
+	See_Also:
+		https://web.archive.org/web/20201029184954/https://powerman.name/Inferno/man/5/0intro.html
+*/
 class Type : StyxObject
 {
 	protected {
@@ -18,44 +32,55 @@ class Type : StyxObject
 		ubyte[] _representation;
 	}
 	
-	// create from value
+	/**
+	A constructor that creates an object of the Type class with the given parameter in the form of STYX_MESSAGE_TYPE. 
+	If called without parameters, then the default parameter is STYX_MESSAGE_TYPE.R_ERROR.
+	Params:
+    type = type of Styx message.
+    
+    Typical usage:
+    ----
+    Type type = new Type(STYX_MESSAGE_TYPE.T_ATTACH);
+    ----
+    */
 	this(STYX_MESSAGE_TYPE type = STYX_MESSAGE_TYPE.R_ERROR)
 	{
 		_type = type;
 		_representation = [cast(ubyte) type];
 	}
 	
-	// getter
+	/// Get as STYX_MESSAGE_TYPE value
 	STYX_MESSAGE_TYPE getType()
 	{
 		return _type;
 	}
 	
-	// setter
+	/// Set from STYX_MESSAGE_TYPE value
 	void setType(STYX_MESSAGE_TYPE type)
 	{
 		_type = type;
 		_representation = [cast(ubyte) type];
 	}
 	
-	// pack to bytes array
+	/// Pack to bytes array
 	ubyte[] pack()
 	{
 		return _representation;
 	}
 	
-	// unpack from bytes array
+	/// Unpack from bytes array
 	void unpack(ubyte[] bytes...)
 	{
 		_representation = [bytes[0]];
 		_type = cast(STYX_MESSAGE_TYPE) bytes[0];
 	}
 	
-	// string representation
+	/// Convenient string representation of an object for printing 
 	override string toString()
 	{
 		return _type.to!string;
 	}
 	
+	/// An alias for easier packing into a byte array without having to manually call the pack() method
 	alias pack this;
 }
