e3903344e08836ff46fe6c84e5a8de8911043c4f
Oleg Bakharev <disconnectix@gmail.com>
2021-10-03 00:55:24 +0300

    Added documentation for lowlevel package

--- source/styx2000/lowlevel/endianness.d
+++ source/styx2000/lowlevel/endianness.d
@@ -1,15 +1,41 @@
+// Written in the D programming language.
+
+/**
+Module for manipulating bytes in constructing and deconstructing various byte orders. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.lowlevel.endianness;
 
-// endianess
+/// Endianess (i.e byte-order)
 enum BYTE_ORDER
 {
+	/// Little-endian byte-order
 	LITTLE_ENDIAN,
+	/// Big-endian byte-order
 	BIG_ENDIAN
 }
 
+
+/**
+	A class that provides functionality for construct and deconstruct values. All methods is static.
+*/
 class EndianSequence
 {	
-	// unpack value from bytes
+	/**
+	Constructs (restores) a value from the passed in byte array.
+    Params:
+	byteOrder = Byte order for value (little or big endian).
+    bytes = Array of unsigned bytes for reconstructing value.
+    
+    Typical usage:
+    ----
+    uint tmp = EndianSequence.unpack!uint([0xab, 0xcd, 0xef, 0x00]);
+    ----
+    */
 	static T unpack(T)(BYTE_ORDER byteOrder, ubyte[] bytes...)
     {
         T mask;
@@ -32,7 +58,17 @@ class EndianSequence
         return mask;
     }
 
-	// pack value to bytes
+	/**
+	Deconstructs (saves) a passed value to unsigned byte array.
+    Params:
+	byteOrder = Byte order for value (little or big endian).
+    bytes = Array of unsigned bytes for reconstructing value.
+    
+    Typical usage:
+    ----
+    ubyte[] tmp = EndianSequence.pack!uint(150_000);
+    ----
+    */
     static ubyte[] pack(T)(BYTE_ORDER byteOrder, T value)
     {
         ubyte[] data;
@@ -59,19 +95,19 @@ class EndianSequence
 }
 
 
-// to little endian bytes
+/// To little endian bytes
 auto toLEBytes(T)(T value)
 {
 	return EndianSequence.pack!T(BYTE_ORDER.LITTLE_ENDIAN, value);
 }
 
-// from little endian bytes
+/// From little endian bytes
 auto fromLEBytes(T)(ubyte[] bytes...)
 {
 	return EndianSequence.unpack!T(BYTE_ORDER.LITTLE_ENDIAN, bytes);
 }
 
-// to big endian bytes
+/// To big endian bytes
 auto toBEBytes(T)(T value)
 {
 	return EndianSequence.pack!T(BYTE_ORDER.BIG_ENDIAN, value);
--- source/styx2000/lowlevel/package.d
+++ source/styx2000/lowlevel/package.d
@@ -1,6 +1,18 @@
+// Written in the D programming language.
+
+/**
+Low-level operation with various objects. 
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.lowlevel;
 
 public {
+	/// Manipulating with bytes in different endianness
 	import styx2000.lowlevel.endianness;
+	/// Working with variable-length fields in 9P / Styx
 	import styx2000.lowlevel.vls;
 }
