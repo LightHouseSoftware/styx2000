3405bcf530c9eb3ce968768775036a8cf2552358
Oleg Bakharev <disconnectix@gmail.com>
2021-10-03 12:06:37 +0300

    Added documentation for encoder module

--- source/styx2000/protomsg/decoder.d
+++ source/styx2000/protomsg/decoder.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+Module for decoding raw bytes to messages of the 9P / Styx protocol.
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protomsg.decoder;
 
 private {
--- source/styx2000/protomsg/encoder.d
+++ source/styx2000/protomsg/encoder.d
@@ -1,3 +1,13 @@
+// Written in the D programming language.
+
+/**
+Module for encoding objects of the 9P / Styx protocol into raw bytes.
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev
+*/
 module styx2000.protomsg.encoder;
 
 private {
@@ -13,7 +23,61 @@ public {
 }
 
 
-// encode styx objects to bytes
+/**
+Encodes a set of 9P / Styx protocol objects into byte representation (array of unsigned bytes)
+Params:
+msg = Array of 9P / Styx protocol objects that represents message.
+
+Typical usage:
+----
+import std.stdio;
+// Example of server message (R_STAT)
+
+// intially, zero size of message
+Size size = new Size;
+// tag (mark foir message)
+Tag tag = new Tag(1);
+// R-message, type: stat
+Type type = new Type(STYX_MESSAGE_TYPE.R_STAT);
+// unique server object
+Qid qid = new Qid(STYX_QID_TYPE.QTDIR, 0, 0);
+
+// chmod 775 (drwxrwxr-x)
+Perm perm = new Perm;
+perm.setPerm(
+	STYX_FILE_PERMISSION.DMDIR | STYX_FILE_PERMISSION.OWNER_EXEC | STYX_FILE_PERMISSION.OWNER_READ | STYX_FILE_PERMISSION.OWNER_WRITE |
+	STYX_FILE_PERMISSION.GROUP_READ | STYX_FILE_PERMISSION.GROUP_WRITE | STYX_FILE_PERMISSION.GROUP_EXEC | 
+	STYX_FILE_PERMISSION.OTHER_READ | STYX_FILE_PERMISSION.OTHER_EXEC 
+);
+	
+// construct stat	
+Stat stat = new Stat(
+	// type and dev for kernel use 
+	77, 4,
+	qid,
+	// permissions
+	perm,
+	// access time (as unix timestamp)
+	0,
+	// modification time (as unix timestamp)
+	0,
+	// conventional length for all directories is 0
+	0,
+	// file name (this, directory name)
+	"test",
+	// user name (owner of file)
+	"duser",
+	// user group name
+	"d_user",
+	// others group name
+	""
+);
+
+// all 9P object in one array with general type
+StyxObject[] msg = cast(StyxObject[]) [size, type, tag, stat];
+msg.encode.writeln;
+----
+*/
 auto encode(StyxObject[] msg)
 {
 	ubyte[] _representation;
