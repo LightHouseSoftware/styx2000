9027f45585a62aff2d8c4b7e142fe3decb9ffa3b
Oleg Bakharev <disconnectix@gmail.com>
2023-02-21 20:42:53 +0300

    Added description for main package

--- source/styx2000/package.d
+++ source/styx2000/package.d
@@ -1,7 +1,12 @@
 module styx2000;
 
 public {
+	/// Main constants of 9P protocol
 	import styx2000.protoconst;
+	
+	/// Entities of 9P protocol
 	import styx2000.protobj;
+	
+	/// All for constructing 9P messages
 	import styx2000.protomsg;
 }
