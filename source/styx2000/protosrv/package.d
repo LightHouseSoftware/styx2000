5215e3389c1532030747d06f530def20d1ff97e6
Oleg Bakharev <disconnectix@gmail.com>
2023-03-14 23:24:25 +0300

    Prepared for new set of modules for working with servers

---
+++ source/styx2000/protosrv/package.d
@@ -0,0 +1,10 @@
+// Written in the D programming language.
+
+/**
+This module contains the minimum implementations of network functionality required for debugging or creating clients/servers.
+
+Copyright: LightHouse Software, 2021
+License:   $(HTTP https://github.com/aquaratixc/ESL-License, Experimental Software License 1.0).
+Authors:   Oleg Bakharev,
+		   Ilya Pertsev   
+*/
